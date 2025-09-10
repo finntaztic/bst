25e592449b2e761bc3cbf50990f806c6aa97e3ab
finntastic <179311210+finntaztic@users.noreply.github.com>
2025-09-10 08:01:01 +0800

    Update index js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 class Node{
-    constructor(data, right, left){
+    constructor(data, right= null, left = null){
         this.data = data;
         this.right = right;
         this.left = left;
@@ -7,12 +7,14 @@ class Node{
 }
 
 class Tree{
-    constructor(root, buildTree = buildTree()){
-    }
-
-    
+    constructor(arr){
+        this.root = this.buildTree(arr);
+    } 
 }
 
-function buildTree(array){
+function buildTree(arr){
 
 }
+
+//second code
+//https://www.geeksforgeeks.org/dsa/sorted-array-to-balanced-bst/
