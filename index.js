d5dbbd0e635961c921c069f4cdbe539713e0be36
finntastic <179311210+finntaztic@users.noreply.github.com>
2025-09-18 08:31:17 +0800

    update index.js

--- index.js
+++ index.js
@@ -11,57 +11,50 @@ class Tree{
         this.root = this.buildTree(arr);
     } 
 
-}
 
-function buildTree(arr){
+    buildTree(arr) {
 
-    //sort array
+            //sort array
     let sortedArr = arr.sort(function(a, b){
         return a-b;
     });
 
-    console.log(sortedArr)
 
     //remove duplicates
     let uniqueArr = sortedArr.filter((item, index) => sortedArr.indexOf(item) === index);
-    console.log(uniqueArr)
 
-    //find the middle element
-    let n = uniqueArr.length;
-    console.log(n)//14
-    if (n === 0)
-        return null;
+        if (uniqueArr.length === 0) return null;
 
+        let mid = Math.floor(uniqueArr.length / 2);
+        let root = new Node(uniqueArr[mid]);
 
-    let mid = Math.floor((n-1)/2);
-    console.log(mid)//6
+        root.left = this.buildTree(uniqueArr.slice(0, mid));
+        root.right = this.buildTree(uniqueArr.slice(mid + 1));
 
-    //creating the root node, its the middle of a sorted array
-    let root = new Node(uniqueArr[mid]);
-    console.log(root)
-    
-    // //get the left half
-    root.left = buildTree(uniqueArr.slice(0, mid));
-    root.right = buildTree(uniqueArr.slice(mid + 1));
-
-    return root
-
-    
-}
-
-const num = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
-const bst = new buildTree(num);
-
-
-function printTree(root, level = 0) {
-    if (root !== null) {
-        printTree(root.right, level + 1);
-        console.log(" ".repeat(4 * level) + "-> " + root.value);
-        printTree(root.left, level + 1);
+        return root;
     }
 }
 
-printTree(bst);
+const prettyPrint = (node, prefix = '', isLeft = true) => {
+  if (node === null) return;
+
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+  }
+
+  console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+  }
+}
+
+const num = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+const bst = new Tree(num);
+prettyPrint(bst.root);
+
+
+
 
 
 //second code
@@ -69,5 +62,5 @@ printTree(bst);
 
 
 //so does the array has to be sorted first? no, some arrays are sorted, most are not, and it doesnt affect the searching. 
-//do i have to remove the duplicates first?  
+//do i have to remove the duplicates first?  yes
 //what does the inorder traversal means? it is mentioned many times in docs and forums
