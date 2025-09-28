efae0bd5590fadf06b6b669aa7227b7d05f4ffcc
finntastic <179311210+finntaztic@users.noreply.github.com>
2025-09-28 10:55:51 +0800

    update index.js

--- index.js
+++ index.js
@@ -19,29 +19,72 @@ class Tree{
   buildTree(array){
     //sort and remove arr
     let arr = [...new Set (array.sort((a, b) => a-b))];
-    console.log(arr)
+    // console.log(`this is arr: ${arr}`)
     
-    let mid = (arr.length -1)/2
+    let mid = Math.floor((arr.length)/2)
+
+    // if (mid < 0){return null}
+    if (arr.length === 0) return null;
+
     console.log(mid)
     let root = new Node(arr[mid]);
-    console.log(root)
+    // console.log(root)
 
+    root.left = this.buildTree(arr.slice (0, mid)); //0, 2
+    root.right = this.buildTree(arr.slice (mid + 1)); 
 
-    root.left = arr.slice (0, mid); //0, 1
-    console.log(root.left)
+    // console.log(root)
 
-
-    // root.left ()
-
-    // let arr = new Set (array);
-    // let removeDup = [...arr];
-    // let sortedDup = removeDup.sort((a, b) => a-b));
+    return root
   }
+
+  insert(root, value){
+    // console.log('helllo')
+    // let root = this.root;
+    // // console.log(root);
+    // console.log(root.data);
+    // console.log(root.left)
+
+
+    if (root === null){
+      return new Node(value)
+    } 
+
+    if (root.data === value)
+      return root;
+
+    if (value < root.data)
+      root.left = insert(root.left, value);
+    else if (value > root.data)
+      root.right = insert(root.right, value);
+
+    return root
+  // }
 }
+}
+
+const prettyPrint = (node, prefix = '', isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+  }
+  console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+  }
+};
+
 
 
 let array = [1, 45, 20, 10, 20, 4, 4];
 let bst = new Tree(array);
+
+prettyPrint(bst.root)
+console.log(bst.root)
+bst.insert(bst.root, 7)
+// prettyPrint(bst.root)
 // bst.buildTree(array);
 
 
@@ -50,3 +93,9 @@ let bst = new Tree(array);
 //--the class Tree is a template, a blueprint. defined what the Tree object should look like
 //so when we call it like bst = new Tree(array), we make an object based off the blueprint that has data in it
 //and we can use the Tree to used on actual data
+
+//2. What is a function called inside a class -- a method
+
+//3. is it a bad practice to have another function inside a method?
+
+//close to what im looking for but not really: https://medium.com/@python-javascript-php-html-css/building-a-binary-search-tree-from-a-javascript-array-374678de88ab
